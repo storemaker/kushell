80ab452df17cee8d6bab1e34b5f1d3f557dbd984
Jakub Taraba <jakub.taraba@gmail.com>
2022-04-23 16:24:54 +0200

    Initial Commit

---
+++ KUSHell/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  KUSHell
+//
+//  Created by Jakub Taraba on 23/04/2022.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
